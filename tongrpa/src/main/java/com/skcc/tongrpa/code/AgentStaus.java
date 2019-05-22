4e735139406ae2223237ffbad20e1a3017959bb9
Administrator <Administrator@SKCC17D00074.SKCC.NET>
2019-05-22 13:00:58 +0900

    등록

---
+++ tongrpa/src/main/java/com/skcc/tongrpa/code/AgentStaus.java
@@ -0,0 +1,7 @@
+package com.skcc.tongrpa.code;
+
+public enum AgentStaus {
+	IDLE, // 유휴상태
+	BUSY, // 작업중
+	DOWN  // 다운됨
+}
---
+++ tongrpa/src/main/java/com/skcc/tongrpa/code/JobStaus.java
@@ -0,0 +1,7 @@
+package com.skcc.tongrpa.code;
+
+public enum JobStaus {
+	REQ, // 실행요청
+	ING, // 실행중
+	CMP //  실행완료
+}
