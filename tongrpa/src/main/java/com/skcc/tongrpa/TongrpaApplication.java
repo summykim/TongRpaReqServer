76d067f4ca38cc5a35e122ef3a7a70b320c7ab5f
Administrator <Administrator@SKCC17D00074.SKCC.NET>
2019-05-23 07:39:02 +0900

    swagger 삭제

--- tongrpa/src/main/java/com/skcc/tongrpa/TongrpaApplication.java
+++ tongrpa/src/main/java/com/skcc/tongrpa/TongrpaApplication.java
@@ -16,11 +16,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.skcc.tongrpa.mq.MqMessageListener;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 
@@ -78,16 +73,5 @@ public class TongrpaApplication {
 		SpringApplication.run(TongrpaApplication.class, args);
 	}
 	
-	@Configuration
-	@EnableSwagger2
-	public class SwaggerConfig {                                    
-	    @Bean
-	    public Docket api() { 
-	        return new Docket(DocumentationType.SWAGGER_2)  
-	          .select()                                  
-	          .apis(RequestHandlerSelectors.any())              
-	          .paths(PathSelectors.any())                          
-	          .build();                                           
-	    }
-	}
+
 }
