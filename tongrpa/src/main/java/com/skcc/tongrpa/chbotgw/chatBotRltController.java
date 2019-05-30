bc0fd063bbc9d86f430fee008714f9ef34e943ae
P116901 <P116901@SKTP116901PD001.SKT.AD>
2019-05-31 08:09:19 +0900

    버그수정

--- tongrpa/src/main/java/com/skcc/tongrpa/chbotgw/chatBotRltController.java
+++ tongrpa/src/main/java/com/skcc/tongrpa/chbotgw/chatBotRltController.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.skcc.tongrpa.agent.agentModel;
 import com.skcc.tongrpa.agent.agentService;
+import com.skcc.tongrpa.jobReq.JobExecReqController;
 import com.skcc.tongrpa.jobReq.JobExecReqModel;
 import com.skcc.tongrpa.jobReq.JobExecReqService;
 @Controller
 public class chatBotRltController {
+	private static final Logger logger = LoggerFactory.getLogger(chatBotRltController.class);
+	
 	@Autowired
 	private JobExecReqService jobReqService;
 	
@@ -59,7 +64,14 @@ public class chatBotRltController {
 		
 		String staDtm="";
 		String endDtm="";
+		if(staDt.length()==10 &&  endDt.length()==10) {
+			staDtm=staDt+" 00:00:00";
+			endDtm=endDt+" 23:59:59";
+		}
+		
+		
 		String today=new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+		
 		if(staDt.length()==0) {
 			staDtm=today+" 00:00:00";
 		}
@@ -67,6 +79,7 @@ public class chatBotRltController {
 			endDtm=today+" 23:59:59";
 		}
 		
+		logger.info(staDtm + " ~ " +endDtm);
 		List<HashMap> statlist = jobReqService.getJobExecReqStat(staDtm, endDtm);
 		model.addAttribute("resultStat",statlist);
 
